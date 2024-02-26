d88c10be692a58b15e2b68112282783394bc2bf3
Pedro Henrique Silva <pedroupandofacil@hotmail.com>
2024-02-26 18:54:57 -0300

    feat: Adicionando o metodo patch aos metodos permitido

--- Administrador/headers.php
+++ Administrador/headers.php
@@ -1,6 +1,6 @@
 <?php
     header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST, OPTIONS, GET, DELETE');
+    header('Access-Control-Allow-Methods: POST, OPTIONS, GET, DELETE, PATCH');
     header('Access-Control-Allow-Headers: Content-Type');
     header('Content-Type: application/json');
 ?>
